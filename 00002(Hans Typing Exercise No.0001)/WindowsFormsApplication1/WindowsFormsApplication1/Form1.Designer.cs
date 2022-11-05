0a2579c4cc8e46ef35afc9a96bb73b5154e4b973
Hans Zhou <116931292+Hans-Zhou-001@users.noreply.github.com>
2022-11-05 19:14:43 +0800

    Add files via upload

---
+++ 00002(Hans Typing Exercise No.0001)/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("思源黑体 HW", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(490, 51);
+            this.label.TabIndex = 0;
+            this.label.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("思源黑体 HW", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(20, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(469, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RESTART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 46F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 155);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(10, 11, 10, 11);
+            this.Name = "Form1";
+            this.Text = "Hans Typing Exercise No.0001";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 00002(Hans Typing Exercise No.0001)/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int which = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //which = 1;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            char press = Convert.ToChar(Convert.ToInt16('a') + which);
+            if (e.KeyChar == press)
+            {
+                char replace = Convert.ToChar( Convert.ToInt16(press)-(Convert.ToInt16('a')-Convert.ToInt16('A')) );
+                label.Text = label.Text.Replace(replace, ' ');
+                which++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            which = 0;
+        }
+    }
+}
